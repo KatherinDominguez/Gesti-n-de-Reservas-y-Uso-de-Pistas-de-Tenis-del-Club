857990a9c7f4be4151d0c792f32b67290894f76c
alejimgithud <almenjim123@gmail.com>
2026-07-25 13:23:57 -0400

    Delete tenis-club directory

--- tenis-club/backend/src/routes/penalizacion.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const c = require("../controllers/penalizacion.controller");
-
-router.get("/", c.obtener);
-router.put("/", c.actualizar);
-
-module.exports = router;
--- tenis-club/backend/src/routes/precio.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const c = require("../controllers/precio.controller");
-
-router.get("/", c.obtener);
-router.put("/", c.actualizar);
-
-module.exports = router;
