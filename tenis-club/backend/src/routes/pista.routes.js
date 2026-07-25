fdd646b501ac2a2d950400fd7f84f0f8be176c00
alejimgithud <almenjim123@gmail.com>
2026-07-25 13:44:29 -0400

    Revert "Feature/hu13 importe cancelaciones"

--- tenis-club/backend/src/routes/pista.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const c = require("../controllers/pista.controller");
-
-router.get("/", c.listar);
-router.get("/:id/disponibilidad", c.disponibilidad);
-
-module.exports = router;
